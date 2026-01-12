4d3df573af0f9def24daf23cf14d6d2576b53838
clement-vitrat <clement.vitrat@campus.academy>
2026-01-12 10:59:38 +0100

    TP Building a REST API with NestJS and Prisma - FINI

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
+
   create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+    return this.prisma.article.create({ data: createArticleDto });
   }
 
   findAll() {
@@ -13,14 +16,21 @@ export class ArticlesService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} article`;
+    return this.prisma.article.findUnique({ where: { id } });
+  }
+
+  findDrafts() {
+    return this.prisma.article.findMany({ where: { published: false } });
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({ where: { id } });
   }
 }
--- src/articles/dto/update-article.dto.ts
+++ src/articles/dto/update-article.dto.ts
@@ -1,3 +1,5 @@
+// src/articles/dto/update-article.dto.ts
+
 import { PartialType } from '@nestjs/swagger';
 import { CreateArticleDto } from './create-article.dto';
 
